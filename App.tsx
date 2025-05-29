c24c32c19612f97dcb110460c72ec397da13bbf7
mmichal93 <mody_m@onet.eu>
2025-05-30 01:58:52 +0200

    Add loading screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { act } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/SlomoMainScreenPNG.png')}
+        style={styles.image}
+      />
+     <ActivityIndicator size={100} color={"#66452a"} style ={styles.activityIndicator}/>
+     <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,4 +22,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  activityIndicator: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    right: 0,
+    bottom: "25%",
+    top: "75%",
+    left: 0, 
+  }
+
 });
